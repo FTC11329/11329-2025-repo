f7ed594378d90ef0efbe4d9e6c266851ff015aba
Allen <allenthomasbrown@gmail.com>
2024-12-15 01:55:39 -0500

    trying some enhanced teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/LocationEnum.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.utility;
+
+public enum LocationEnum {
+    leftSideSub, basket, frontSub, observation, rightSideSub
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/PoseFunctions.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.utility;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+
+public class PoseFunctions {
+    private static final double sideSubY = 24;
+
+    private static final double basketX = -24;
+    private static final double observationX = 24;
+
+    public static LocationEnum getLocation(Pose currentPose) {
+        if (currentPose.getY() < sideSubY) {
+            if (currentPose.getX() > 0) {
+                return LocationEnum.rightSideSub;
+            } else {
+                return LocationEnum.leftSideSub;
+            }
+        } else {
+            if (currentPose.getX() < basketX) {
+                return LocationEnum.basket;
+            } else if (currentPose.getX() < observationX) {
+                return LocationEnum.frontSub;
+            } else {
+                return LocationEnum.observation;
+            }
+        }
+    }
+}
