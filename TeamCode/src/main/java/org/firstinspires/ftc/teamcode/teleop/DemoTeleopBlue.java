2c039b65f82f2a6939c236614ecb30c2c3394c15
Adrian Ross <Adrian.michael.ross08@gmail.com>
2025-06-02 18:46:25 -0400

    Demo op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DemoTeleopBlue.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.utility.RobotSideEnum;
+
+@TeleOp(name = "Demo Tele-op Blue", group = " Comp mode")
+public class DemoTeleopBlue extends OpMode {
+    DemoTeleop teleop;
+    @Override
+    public void init() {
+        teleop = new DemoTeleop(hardwareMap, telemetry, gamepad1, gamepad2, RobotSideEnum.Blue);
+        teleop.init();
+    }
+
+    @Override
+    public void loop() {
+        teleop.loop();
+    }
+
+    @Override
+    public void start() {
+        teleop.start();
+    }
+
+    @Override
+    public void stop() {
+        teleop.stop();
+    }
+}
