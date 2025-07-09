865741cc12cb7b7312495dd3894ccc7d7a9273c6
dorongordon <doron4gordon@gmail.com>
2025-07-09 17:23:15 -0400

    Auto Replay Class!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/NewTeleopBlueSecret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/NewTeleopBlueSecret.java
@@ -13,6 +13,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.autos.AutoReplay;
 import org.firstinspires.ftc.teamcode.pedropathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.pedropathing.follower.FollowerConstants;
 import org.firstinspires.ftc.teamcode.pedropathing.localization.Pose;
@@ -173,20 +174,7 @@ public class NewTeleopBlueSecret {
     boolean unjamAfterIntake = false;
     double unjamAfterIntakeTime = 2000000000;
 
-    PressHold recording;
-    PressHold replay;
-    PressHold pointerInput;
-    double lastTimer = 0;
-    Pose lastPose = new Pose(0, 0, 0);
-    double deltaTime = 0.1;
-    double deltaError = 2;
-    int replayIndex = 0;
-    StateEntryJson currentReplayStates;
-    PathChain replayPath;
-    GamepadStateEntry gamepadDelta1;
-    GamepadStateEntry gamepadDelta2;
-    int logPointer = 0;
-
+    AutoReplay autoReplay;
 
     //this is here because I have to have a teleop blue and teleop red
     HardwareMap hardwareMap;
@@ -223,10 +211,9 @@ public class NewTeleopBlueSecret {
         driveTrain = new Drivetrain(hardwareMap);
 
         stateMachine = new StateMachine();
-        recording = new PressHold(PressHold.PressType.DoublePress);
-        replay = new PressHold(PressHold.PressType.DoublePress);
-        pointerInput = new PressHold(PressHold.PressType.LongPress);
 
+        autoReplay = new AutoReplay(follower, telemetry, gamepad1, gamepad2);
+        autoReplay.init();
 
         //Building paths
         toFrontWall = new Path(new BezierCurve(new Point(placeSub), new Point(controlPointForWall1), new Point(controlPointForWall2), new Point(frontWall)));
@@ -236,12 +223,6 @@ public class NewTeleopBlueSecret {
 
         placeSubPath = new Path(new BezierCurve(new Point(pickupWall), new Point(controlPointForSubPlace), new Point(placeSub)));
         placeSubPath.setConstantHeadingInterpolation(placeSub.getHeading());
-
-        currentReplayStates = new StateEntryJson();
-
-        loadPointer();
-        telemetry.addData("pointer: ", logPointer);
-        telemetry.update();
     }
 
     public void start() {
@@ -252,95 +233,7 @@ public class NewTeleopBlueSecret {
         elapsedTime.reset();
     }
 
-    public void recordPositions() {
-        File dir = AppUtil.getInstance().getSettingsFile("TeamCodeLogs").getParentFile();
-        File file = new File(dir, "movement" + logPointer + ".json");
-        telemetry.addData("Recording, here: ", true);
-        try (FileWriter writer = new FileWriter(file)) {
-//            if (file.exists()) {
-//                FileReader reader = new FileReader(file);
-//                BufferedReader bufferedReader = new BufferedReader(reader);
-//                StringBuilder content = new StringBuilder();
-//                String line;
-//                while ((line = bufferedReader.readLine()) != null) {
-//                    content.append(line);
-//                }
-//                bufferedReader.close();
-//                log = new JSONArray(new JSONTokener(content.toString()));
-//                reader.close();
-//            } else {
-//            }
-            Gson gson = new GsonBuilder().create();
 
-            writer.write(gson.toJson(currentReplayStates));
-            writer.close();
-
-            telemetry.addData("Drive log written", true);
-        } catch (Exception e) {
-            telemetry.addData("Drive log error", e.getMessage());
-        }
-    }
-
-    public void savePointer() {
-        File dir = AppUtil.getInstance().getSettingsFile("TeamCodeLogs").getParentFile();
-        File file = new File(dir, "pointer.json");
-        try (FileWriter writer = new FileWriter(file)) {
-            Gson gson = new GsonBuilder().create();
-
-            writer.write(gson.toJson(new PointerJson(logPointer)));
-            writer.close();
-
-            telemetry.addData("Pointer Saved written:", true);
-        } catch (Exception e) {
-            telemetry.addData("Pointer error", e.getMessage());
-        }
-    }
-
-    public void loadPoses() {
-        File dir = AppUtil.getInstance().getSettingsFile("TeamCodeLogs").getParentFile();
-        File file = new File(dir, "movement" + logPointer + ".json");
-
-        if (!file.exists()) return;
-
-        StringBuilder jsonBuilder = new StringBuilder();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                jsonBuilder.append(line);
-            }
-        } catch (Exception e) {
-            telemetry.addData("Failed to Load", true);
-        }
-
-        String jsonString = jsonBuilder.toString();
-
-        Gson gson = new GsonBuilder().create();
-        currentReplayStates = gson.fromJson(jsonString, StateEntryJson.class);
-    }
-
-    public void loadPointer() {
-        File dir = AppUtil.getInstance().getSettingsFile("TeamCodeLogs").getParentFile();
-        File file = new File(dir, "pointer.json");
-
-        if (!file.exists()) return;
-
-        StringBuilder jsonBuilder = new StringBuilder();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                jsonBuilder.append(line);
-            }
-        } catch (Exception e) {
-            telemetry.addData("Failed to Load", true);
-        }
-
-        String jsonString = jsonBuilder.toString();
-
-        Gson gson = new GsonBuilder().create();
-        logPointer = gson.fromJson(jsonString, PointerJson.class).pointer;
-    }
 
     public void loop() {
         follower.update();
@@ -349,95 +242,12 @@ public class NewTeleopBlueSecret {
         driveForward = -gamepad1.left_stick_y;
         driveStrafe = -gamepad1.left_stick_x;
         driveRotation = -gamepad1.right_stick_x;
-        recording.checkStatus(gamepad1.a);
-        replay.checkStatus(gamepad1.b);
-        pointerInput.checkStatus(gamepad1.left_bumper);
 
-        telemetry.addData("LOG POINTER", logPointer);
+        autoReplay.update();
+        gamepad1 = autoReplay.getGamepad1();
+        gamepad2 = autoReplay.getGamepad2();
 
-        telemetry.addData("Follower Busy", follower.isBusy());
-        telemetry.addData("Replay Path Null", replayPath == null);
-        telemetry.addData("recording is on", recording.isOn);
-        telemetry.addData("replay is on", replay.isOn);
-        telemetry.addData("replay start", replay.startPress);
-        telemetry.addData("Following Path: Replay Index: ", replayIndex);
-
-        for (int i = 0; i < currentReplayStates.size; i++){
-            telemetry.addData("t", (currentReplayStates.timeList.get(i)));
-            telemetry.addData("pos-x", (currentReplayStates.poseList.get(i).x));
-            telemetry.addData("pos-y", (currentReplayStates.poseList.get(i).y));
-        }
-        telemetry.addData("Size of List: ", currentReplayStates.size);
-
-        if (pointerInput.startPress) logPointer = 0;
-        if (pointerInput.isOn) logPointer = (int) Math.floor(pointerInput.time.seconds());
-        if (pointerInput.endPress) savePointer();
-
-        if(recording.startPress){
-            currentReplayStates = new StateEntryJson();
-            lastTimer = 0;
-            lastPose = follower.getPose();
-            gamepadDelta1 = new GamepadStateEntry(gamepad1);
-            gamepadDelta2 = new GamepadStateEntry(gamepad2);
-        }
-        if (recording.isOn) {
-            telemetry.addData("Error Mag: ", MathFunctions.distance(lastPose, follower.getPose()));
-            if (MathFunctions.distance(lastPose, follower.getPose()) > deltaError) {
-                currentReplayStates.timeList.add(recording.time.seconds());
-                currentReplayStates.poseList.add(new PoseStateEntry(follower.getPose()));
-                currentReplayStates.gamepad1List.add(gamepadDelta1);
-                currentReplayStates.gamepad2List.add(gamepadDelta2);
-                currentReplayStates.size += 1;
-                lastPose = follower.getPose();
-                gamepadDelta1 = new GamepadStateEntry(gamepad1);
-                gamepadDelta2 = new GamepadStateEntry(gamepad2);
-            }else{
-                gamepadDelta1.mergeBooleans(new GamepadStateEntry(gamepad1));
-                gamepadDelta2.mergeBooleans(new GamepadStateEntry(gamepad2));
-            }
-        }
-        if (recording.endPress){
-            recordPositions();
-        }
-        if (replay.startPress){
-            loadPoses();
-            if (currentReplayStates.size > 1){
-                ArrayList<Path> path = new ArrayList<>();
-                Pose cPos = follower.getPose();
-                PoseStateEntry sPos = currentReplayStates.poseList.get(0);
-                path.add(follower.linearPathBuilder(
-                        new Pose(cPos.getX(), cPos.getY(), cPos.getHeading()),
-                        new Pose(sPos.x, sPos.y, sPos.heading)));
-                for (int i = 0; i < currentReplayStates.size - 1; i++){
-                    PoseStateEntry pos1 = currentReplayStates.poseList.get(i);
-                    PoseStateEntry pos2 = currentReplayStates.poseList.get(i + 1);
-                    path.add(follower.linearPathBuilder(
-                            new Pose(pos1.x, pos1.y, pos1.heading),
-                            new Pose(pos2.x, pos2.y, pos2.heading)));
-                    telemetry.addData("Pose " + i, "x: " + pos1.x + " y: " + pos1.y + " heading: " + pos1.heading);
-                }
-                replayPath = new PathChain(path);
-                follower.followPath(replayPath);
-                /*ArrayList<Point> path = new ArrayList<>();
-                for (int i = 0; i < currentReplayStates.size; i++) {
-                    PoseStateEntry pos = currentReplayStates.poseList.get(i);
-                    path.add(new Point(new Pose(pos.x, pos.y, pos.heading)));
-                }
-                follower.followPath(new Path(new BezierCurve(path)));*/
-            }
-        }
-        if (replay.isOn){
-            replayIndex = follower.getCurrentPathNumber();
-            gamepad1 = currentReplayStates.gamepad1List.get(replayIndex).convertToGamepad();
-            gamepad2 = currentReplayStates.gamepad2List.get(replayIndex).convertToGamepad();
-            follower.telemetryDebug(telemetry);
-        }
-        if (replay.endPress){
-            follower.breakFollowing();
-        }
-        telemetry.addData("Hello World", true);
         //telemetry.addData("path: ", currentReplayPath);
-        telemetry.addData("Replay", replay);
         if (gamepad1.right_bumper) {
             driveSpeed = DriveSpeedEnum.Fast;
         } else {
@@ -477,7 +287,7 @@ public class NewTeleopBlueSecret {
         unJam = gamepad2.circle;//b
 
         //Drivetrain ******************************************************************************~
-        if (!climberActive && !autoMovement && !climbPause && !replay.isOn) {
+        if (!climberActive && !autoMovement && !climbPause && !autoReplay.IsReplayOn()) {
             if (climbToggButton) {
                 climberActive = true;
                 climbDebounce = true;
@@ -986,113 +796,6 @@ public class NewTeleopBlueSecret {
             this.pose = pose;
             this.gamepad1State = gamepad1State;
             this.gamepad2State = gamepad2State;
-        }
-    }
-
-    public static class GamepadStateEntry {
-        public boolean a, b, x, y;
-        public boolean dpad_up, dpad_down, dpad_left, dpad_right;
-        public boolean left_bumper, right_bumper;
-        public boolean left_stick_button, right_stick_button;
-        public float left_stick_x, left_stick_y;
-        public float right_stick_x, right_stick_y;
-        public float left_trigger, right_trigger;
-
-        public GamepadStateEntry(Gamepad g) {
-            this.a = g.a;
-            this.b = g.b;
-            this.x = g.x;
-            this.y = g.y;
-            this.dpad_up = g.dpad_up;
-            this.dpad_down = g.dpad_down;
-            this.dpad_left = g.dpad_left;
-            this.dpad_right = g.dpad_right;
-            this.left_bumper = g.left_bumper;
-            this.right_bumper = g.right_bumper;
-            this.left_stick_button = g.left_stick_button;
-            this.right_stick_button = g.right_stick_button;
-            this.left_stick_x = g.left_stick_x;
-            this.left_stick_y = g.left_stick_y;
-            this.right_stick_x = g.right_stick_x;
-            this.right_stick_y = g.right_stick_y;
-            this.left_trigger = g.left_trigger;
-            this.right_trigger = g.right_trigger;
-        }
-
-        public Gamepad convertToGamepad() {
-            Gamepad g = new Gamepad();
-
-            // Buttons
-            g.a = this.a;
-            g.b = this.b;
-            g.x = this.x;
-            g.y = this.y;
-            g.dpad_up = this.dpad_up;
-            g.dpad_down = this.dpad_down;
-            g.dpad_left = this.dpad_left;
-            g.dpad_right = this.dpad_right;
-            g.left_bumper = this.left_bumper;
-            g.right_bumper = this.right_bumper;
-            g.left_stick_button = this.left_stick_button;
-            g.right_stick_button = this.right_stick_button;
-
-            // Joysticks
-            g.left_stick_x = this.left_stick_x;
-            g.left_stick_y = this.left_stick_y;
-            g.right_stick_x = this.right_stick_x;
-            g.right_stick_y = this.right_stick_y;
-
-            // Triggers
-            g.left_trigger = this.left_trigger;
-            g.right_trigger = this.right_trigger;
-
-            return g;
-        }
-
-        public void mergeBooleans(GamepadStateEntry other) {
-            this.a &= other.a;
-            this.b &= other.b;
-            this.x &= other.x;
-            this.y &= other.y;
-
-            this.dpad_up &= other.dpad_up;
-            this.dpad_down &= other.dpad_down;
-            this.dpad_left &= other.dpad_left;
-            this.dpad_right &= other.dpad_right;
-
-            this.left_bumper &= other.left_bumper;
-            this.right_bumper &= other.right_bumper;
-
-            this.left_stick_button &= other.left_stick_button;
-            this.right_stick_button &= other.right_stick_button;
-
-            // Floats remain unchanged
-        }
-    }
-
-    public static class PoseStateEntry {
-        public double x, y, heading;
-
-        public PoseStateEntry(Pose pose) {
-            this.x = pose.getX();
-            this.y = pose.getY();
-            this.heading = pose.getHeading();
-        }
-    }
-
-    public static class StateEntryJson {
-        public int size = 0;
-        public List<Double> timeList = new ArrayList<>();
-        public List<PoseStateEntry> poseList = new ArrayList<>();
-        public List<GamepadStateEntry> gamepad1List = new ArrayList<>();
-        public List<GamepadStateEntry> gamepad2List = new ArrayList<>();
-    }
-
-    public static class PointerJson {
-        public int pointer = 0;
-
-        public PointerJson(int pointer){
-            this.pointer = pointer;
         }
     }
 }
