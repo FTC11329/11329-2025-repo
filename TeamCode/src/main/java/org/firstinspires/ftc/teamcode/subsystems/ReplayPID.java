80f609fcfdba8a95f35796e705ece3e37eb29166
Adrian Ross <Adrian.michael.ross08@gmail.com>
2025-08-29 13:22:16 -0400

    added I term in the replay PID
    works by adding up the error over some delta T to the past error so you never actually need to perform any integral operations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ReplayPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ReplayPID.java
@@ -6,6 +6,7 @@ import org.firstinspires.ftc.teamcode.pedropathing.localization.Pose;
 public class ReplayPID {
     AutoReplayAllenTest autoReplay;
 
+    double integralHistory, previousTime;
     public double[] replayPIDMotorValues(double currentTime, Pose currentPose){
         double[] targetList = autoReplay.getTargets(currentTime); //xTarget 0, yTarget 1, headingTarget 2, vxTarget 3, vyTarget 4, omegaTargeta 5, xTarget 6, ayTarget 7, alphaTarget 8
         //first for the feed forward system that will predict motor powers to reach target acceleration and velocity
@@ -16,8 +17,9 @@ public class ReplayPID {
         double ffTheta = kV * targetList[5] + kA * targetList[8];
 
         double[] errorList = autoReplay.getError(currentTime, currentPose);
-        double kP = 1.0, kI = 0.0, kD = 0.0;
-        double pidX = kP * errorList[0] + kI * errorList[0] + kD * errorList[0];
+        double dT = currentTime - previousTime;
+        double kP = 1.0, kI = 0.1, kD = 0.0, integralGain = 0.5; // the D term probably won't do much for this controller so I won't use it
+        double pidX = kP * errorList[0] + kI * (integralHistory + integralGain * errorList[0] * dT) + kD * errorList[0];
         double pidY = kP * errorList[1] + kI * errorList[1] + kD * errorList[1];
         double pidTheta = kP * errorList[2] + kI * errorList[2] + kD * errorList[2];
 
